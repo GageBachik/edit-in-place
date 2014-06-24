6f2386438c9d33e0eabf11bb5530e039164b3ba4
gbachik <gbachik@gmail.com>
2014-06-24 11:57:57 -0600

    Finished challeneg working on enter to submit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	$('.box-static').on('click', function(){
+		var currentText = $(this).text();
+		$(this).toggle();
+		$('.box-editable textarea').text(currentText);
+		$('.box-editable').toggle();
+	});
+
+	$('.save').on('click', function(){
+		var currentText = $('.box-editable textarea').val();
+		$('.box-static').text(currentText);
+		$('.box-static').toggle();
+		$('.box-editable').toggle();
+	});
+
+	$('.cancel').on('click', function(){
+		$('.box-static').toggle();
+		$('.box-editable').toggle();
+	});
+
+});
